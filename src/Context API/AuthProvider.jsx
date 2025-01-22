72dc0e7d4680c156a5697bf86de55c4f7ff464b6
Shadmans-World <shaon10516045@gmail.com>
2025-01-22 20:46:11 +0600

    Context added to the main.jsx

--- src/Context API/AuthProvider.jsx
+++ src/Context API/AuthProvider.jsx
@@ -1,10 +1,14 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const AuthContext = createContext(null)
 const AuthProvider = ({children}) => {
 
+    const [user, setUser] = useState([])
+    const [loading, setLoading] = useState(true)
     const authInfo = {
 
+        user,
+        loading
     }
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Routers/Routers.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routers/Routers.jsx";
+import AuthProvider, { AuthContext } from "./Context API/AuthProvider.jsx";
 
-
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className='max-w-[1440px] mx-auto'>
-    <RouterProvider router={router}/>
-    </div>
-  </StrictMode>,
-)
+    <AuthProvider>
+      <div className="max-w-[1440px] mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
+  </StrictMode>
+);
